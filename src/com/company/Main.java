6a07707b9aab1f1f474b7e39932124da25d4c1e6
borowik10 <57679810+borowik10@users.noreply.github.com>
2019-11-12 19:55:23 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Publikujemy pierwszy program");
+ 
+        System.out.println("Edycja przez wspolpracownika");
     }
 }
