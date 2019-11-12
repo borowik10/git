14e29f7659ec8ca325aa4f5aeb2c2367c033bc3d
boro <pawel.borowicki@gmail.com>
2019-11-12 20:20:26 +0100

    dodanie nowej funkcjonalnosci

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("kolejna zmiana");
         System.out.println("Edycja przez wspolpracownika");
         // konflik rozwiazany
+        System.out.println("Nowa funkcjonalnosc projektu");
     }
 }
